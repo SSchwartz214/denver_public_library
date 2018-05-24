78da50a417e0ace0fd791b04c193f4c173f7a1e7
Seth Schwartz <sschwartz214@gmail.com>
2018-05-24 10:51:32 -0600

    initial commit

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,16 @@
 class Author
+  attr_reader :first_name,
+              :last_name,
+              :books
+
+  def initialize(author)
+    @first_name = author[:first_name]
+    @last_name = author[:last_name]
+    @books = []
+  end
+
+  def add_book(title, publication_date)
+    @books << Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
+  end
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,38 @@
 require './test/test_helper'
+require './lib/author'
+require './lib/book'
 
 class AuthorTest < Minitest::Test
+  def test_it_exists
+    book = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
+    assert_instance_of Author, book
+  end
 
+  def test_it_has_attributes
+    author = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal "Charlotte", author.first_name
+    assert_equal "Bronte", author.last_name
+  end
+
+  def test_it_starts_with_no_books
+    author = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal [], author.books
+  end
+
+  def test_it_can_add_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    book_1 = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: "Jane Eyre", publication_date: "October 16, 1847"})
+    book_2 = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: "Villette", publication_date: "1853"})
+
+    charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, charlotte_bronte.books.last
+
+    charlotte_bronte.add_book("Villette", "1853")
+
+    assert_instance_of Book, charlotte_bronte.books.last
+  end
 end
