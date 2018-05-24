7c22d0f9d66a6dfd12aff8e59d863889765f2e72
Seth Schwartz <sschwartz214@gmail.com>
2018-05-24 12:01:04 -0600

    adds find by author and publication date methods

--- lib/library.rb
+++ lib/library.rb
@@ -15,4 +15,21 @@ class Library
     end
   end
 
+  def author_name(book)
+    "book.author_first_name + book.author_last_name"
+  end
+
+  def find_by_author
+    @books.find_all do |book|
+      author_name(author) == "Charlotte Bronte"
+    end
+  end
+
+  def find_by_publication_date
+    @books.find_all do |book|
+      book.publication_date == "1960"
+    end
+  end
+
+
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -62,6 +62,7 @@ class LibraryTest < Minitest::Test
   end
 
   def test_it_can_alphabetize_books
+    skip
     dpl = Library.new
 
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
@@ -76,6 +77,30 @@ class LibraryTest < Minitest::Test
     dpl.add_to_collection(villette)
 
     assert_equal [jane_eyre, villette, mockingbird], dpl.card_catalogue
+  end
+
+  def test_it_can_search_by_author
+    skip
+    dpl = Library.new
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    assert_equal
+  end
+
+  def test_it_can_search_by_publication_date
+  skip
+    dpl = Library.new
+
   end
 
 
